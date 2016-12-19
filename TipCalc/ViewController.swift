5bff2d412d61c3ee0c8e8680004e2c1788fd0b07
SzuKaiYang <bbkai2857@gmail.com>
2016-12-19 09:09:25 -0600

    TipCalc Upload

--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  TipCalc
 //
-//  Created by YangSzu Kai on 2016/12/17.
+//  Created by Yang Szu Kai on 2016/12/17.
 //  Copyright © 2016年 YangSzu Kai. All rights reserved.
 //
 
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TipLabel: UILabel!
+    
+    @IBOutlet weak var billFiled: UITextField!
+    
+    @IBOutlet weak var Total: UILabel!
+    
+    @IBOutlet weak var tipRateSelect: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func USER_TAB(_ sender: Any) {
+        
+        view.endEditing(true) //End Keyboard Editing
+    }
+
+    @IBAction func CalcTip(_ sender: Any) {
+        
+        let PERCENT_RATE = [0.15, 0.18, 0.25]
+        
+        let bill = Double(billFiled.text!) ?? 0
+        //Convert to Numbers Set default to 0 if it is weired value
+        
+        let tip = bill * PERCENT_RATE[tipRateSelect.selectedSegmentIndex]
+        // [Means: Which index of UISegmentControl choosed start from 0,1,2]
+        
+        let total = bill + tip
+
+        TipLabel.text = String(format: "$%.2f", tip) //%f double or float .2 two decimal place
+        Total.text = String(format: "$%.2f", total)
+
+    }
+    
+    @IBAction func ChangeRate(_ sender: Any) {
+        CalcTip(true)
+        
+    }
 
 }
 
